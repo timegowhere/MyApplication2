a80f8136807b4dec15b9623441fd00cec95d07e4
timegowhere <1653942463@qq.com>
2016-10-11 14:47:06 +0800

    第二次提交

--- app/src/main/java/com/sample/myapplication/MainActivity.java
+++ app/src/main/java/com/sample/myapplication/MainActivity.java
@@ -8,6 +8,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
